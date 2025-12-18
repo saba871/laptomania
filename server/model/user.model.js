9eed75d1a45c218aff5051f1ae9c2019c4866c86
saba871 <sabatabatadze40@gmail.com>
2025-12-18 07:26:50 -0800

    cookie

--- server/model/user.model.js
+++ server/model/user.model.js
@@ -6,66 +6,66 @@ const crypto = require("crypto")
 
 // მომხმარებლის მოდელი
 const userSchema = new mongoose.Schema({
-  fullname: {
-    type: String,
-    required: [true, "User fullname is required"],
-    lowercase: true
-  },
+    fullname: {
+        type: String,
+        required: [true, "User fullname is required"],
+        lowercase: true
+    },
 
-  email: {
-    type: String,
-    required: [true, "User email is required"],
-    unique: true,
-    lowercase: true,
-    validate: [validator.isEmail, "Please provide a valid email"]
-  },
+    email: {
+        type: String,
+        required: [true, "User email is required"],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, "Please provide a valid email"]
+    },
 
-  password: {
-    type: String,
-    required: [ true, "User password is required"],
-    minlength: [4, "Password must be at least 4 characters"],
-    maxlength: [12, "Password must be at most 12 characters"],
-    select: false
-  },
+    password: {
+        type: String,
+        required: [true, "User password is required"],
+        minlength: [4, "Password must be at least 4 characters"],
+        maxlength: [12, "Password must be at most 12 characters"],
+        select: false
+    },
 
-  role: {
-    type: String,
-    enum: ["user", "admin", "moderator"],
-    default: "user"
-  },
+    role: {
+        type: String,
+        enum: ["user", "admin", "moderator"],
+        default: "user"
+    },
 
-  isActive: {
-    type: Boolean,
-    default: true
-  },
+    isActive: {
+        type: Boolean,
+        default: true
+    },
 
-  varificationCode: String,
+    varificationCode: String,
 
-  isVerified: {
-    type: Boolean,
-    default: false // true
-  },
+    isVerified: {
+        type: Boolean,
+        default: false // true
+    },
 
 }, { timestamps: true })
 
 
 // შექმნის მომენტში სანამ დასეივდება მომხმარებლის info მანამდე გაეშვება ეს კოდი რომელიც მოახდენს password-ის hashing-ს
-userSchema.pre("save", async function() {
-  if (!this.isModified("password")) return; // async function-ში არ ვიძახებ next()
-  this.password = await bcrypt.hash(this.password, 12);
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return; // async function-ში არ ვიძახებ next()
+    this.password = await bcrypt.hash(this.password, 12);
 });
 
 
 // ადარებს logIn ის დროს მომხმარებლის პაროლებს და აბრუენებს true ან false
-userSchema.methods.comparePassword = async function(candidate) {
-  return await bcrypt.compare(candidate, this.password)
+userSchema.methods.comparePassword = async function (candidate) {
+    return await bcrypt.compare(candidate, this.password)
 }
 
 
-userSchema.methods.createEmailVerificationToken = function() {
-  const code = crypto.randomBytes(12).toString("hex");
-  this.varificationCode = code
-  return code
+userSchema.methods.createEmailVerificationToken = function () {
+    const code = crypto.randomBytes(12).toString("hex");
+    this.varificationCode = code
+    return code
 }
 
 
@@ -75,10 +75,10 @@ userSchema.methods.createEmailVerificationToken = function() {
 // 2. secret key => google-იდან random key
 // სეთინგები: რამდენ ხანში ამოიწუროს ვადა token-ის
 
-userSchema.methods.signToken = function() {
-  return jwt.sign({ id: this._id}, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  })
+userSchema.methods.signToken = function () {
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    })
 }
 
 const User = mongoose.model("User", userSchema);
